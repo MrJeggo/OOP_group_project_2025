e06aac7f6b25001801b52a8dfe88aba24b43ab7d
pizzapete12345 <thul_peter@icloud.com>
2025-11-05 14:56:22 +0800

    updated the describtions and added 2 health item

--- Item.py
+++ Item.py
@@ -26,19 +26,28 @@ class Miscellaneous(Item):
 
 pencil = Weapon()
 pencil.name = "unsharpend pencil"
-pencil.description = "a great weapon worn down by use"
+pencil.description = "you dealt 10 damage not great not terrible"
 pencil.effective_against = []
 pencil.attack_power = 10
 pencil.ineffective_against = []
 
 sharpend_pencil = Weapon()
 sharpend_pencil.name = "pencil"
-sharpend_pencil.description = "an elagent weapon for a more civilized age"
+sharpend_pencil.description = "you dealt 15 damage better than 10"
 sharpend_pencil.effective_against = []
 sharpend_pencil.attack_power = 15
 sharpend_pencil.ineffective_against = []
 
 pencil_sharperner = Miscellaneous()
 pencil_sharperner.name = "pencil sharperner"
-pencil_sharperner.description = "everyone knows what a pencil sharperner is"
+pencil_sharperner.description = "everyone knows what a pencil sharperner does"
 
+popcorn = Consumable()
+popcorn.name = "pocorn kernal"
+popcorn.description = "you ate the kernal you gain 5 health"
+popcorn.heal_amount = 1
+
+apple = Consumable()
+apple.name = "apple"
+apple.description = "an apple a day just gave you 50 health"
+apple.heal_amount = 50
