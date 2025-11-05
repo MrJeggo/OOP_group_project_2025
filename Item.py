27f65d76e637eac8d1e235683067ac880f2ef3da
pizzapete12345 <thul_peter@icloud.com>
2025-11-05 14:41:56 +0800

    Added 3 items (this somehow took 30 minutes)

--- Item.py
+++ Item.py
@@ -22,4 +22,23 @@ class Miscellaneous(Item):
     def __init__(self, name, description):
         super().__init__(name, description)
 
+# here be items matey
+
+pencil = Weapon()
+pencil.name = "unsharpend pencil"
+pencil.description = "a great weapon worn down by use"
+pencil.effective_against = []
+pencil.attack_power = 10
+pencil.ineffective_against = []
+
+sharpend_pencil = Weapon()
+sharpend_pencil.name = "pencil"
+sharpend_pencil.description = "an elagent weapon for a more civilized age"
+sharpend_pencil.effective_against = []
+sharpend_pencil.attack_power = 15
+sharpend_pencil.ineffective_against = []
+
+pencil_sharperner = Miscellaneous()
+pencil_sharperner.name = "pencil sharperner"
+pencil_sharperner.description = "everyone knows what a pencil sharperner is"
 
