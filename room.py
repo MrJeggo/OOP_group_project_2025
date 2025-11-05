5db9728685c3c429301d9bcfce8c1e8590f490a5
sch312413 <sch312413@gmail.com>
2025-11-05 14:20:53 +0800

    added room class

---
+++ room.py
@@ -0,0 +1,17 @@
+from teachers import Teacher
+
+class Room():
+    def __init__(self, room_num, is_lock, room_direction, teacher_inside, item_inside, room_descriptor, is_shop):
+        self.room_num = room_num
+        self.is_lock = is_lock
+        self.room_direction = room_direction
+        self.teacher_inside = teacher_inside
+        self.item_inside = item_inside
+        self.room_description = room_direction
+        self.is_shop = is_shop
+
+    def which_teacher(teacher_inside):
+        
+        return 
+
+        
