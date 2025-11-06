3e2f16f29cdaad5cda424a561414fe523bd5fbd6
pizzapete12345 <thul_peter@icloud.com>
2025-11-06 14:18:19 +0800

    added a few new items

--- Item.py
+++ Item.py
@@ -24,16 +24,28 @@ class Miscellaneous(Item):
 
 # here be items matey
 
+# weapons stored here
+
 pencil = Weapon("unsharpend pencil","you dealt 10 damage not great not terrible",10,[],[])
 
+calculator = Weapon("scientific calculator","you subtraced 13 health from your foes",13,[],[])
+
+bettercalculator=Weapon("Graphic calculator","you subtracted 10*2 health",20,[],[])
 
 sharpend_pencil = Weapon("pencil","you dealt 15 damage better than 10",15,[],[])
 
+# miscellaneous items
 
 pencil_sharperner = Miscellaneous("pencil sharperner","everyone knows what a pencil sharperner does")
 
+# keys 
+
+
+
+# consumables
 
 popcorn = Consumable("pocorn kernal","you ate the kernal you gain 5 health",5)
 
+milk = Consumable("Milk","you drank the milk you gain 20 health")
 
 apple = Consumable("apple","an apple a day just gave you 50 health",50)
