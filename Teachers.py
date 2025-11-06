3a3f687374cab4466b78bfe73caf4c7507e39239
FRH09 <hoffmann.ff.felix@gmail.com>
2025-11-06 13:54:15 +0800

    resethealth

--- Teachers.py
+++ Teachers.py
@@ -1,3 +1,4 @@
+import Player 
 class teacher: 
     def __init__(self,name, maxhealth,attackdamage,subject):
         self._name = name
@@ -6,10 +7,14 @@ class teacher:
         self.attackdamage = attackdamage
         self.subject = subject
 
-    def heal(self,maxhealth):
+    def heal(self):
         if self.health < self._maxhealth*0.9:
             self.health *= 1.1
             return f"The {self._name} has healthed by 10%"
+        
+    def resethealth(self):
+        if Player.is_dead() == True: 
+            Player self.health = self._maxhealth
   
     def getName(self):
         return self._name
