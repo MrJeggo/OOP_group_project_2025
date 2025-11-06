d4425e3c8e1216b04b17ecbf19a7206394856514
FRH09 <hoffmann.ff.felix@gmail.com>
2025-11-06 14:49:07 +0800

    nihao

---
+++ teachers.py
@@ -0,0 +1,36 @@
+from player import Player
+class teacher: 
+    def __init__(self,name, maxhealth,attackdamage,subject):
+        self._name = name
+        self.health = maxhealth 
+        self._maxhealth = maxhealth
+        self.attackdamage = attackdamage
+        self.subject = subject
+
+    def heal(self):
+        if self.health < self._maxhealth*0.9:
+            self.health *= 1.1
+            return f"The {self._name} has healthed by 10%"
+        
+    def resethealth(self):
+        if Player.is_dead() == True: 
+            self.health = self._maxhealth
+  
+    def getName(self):
+        return self._name
+    
+    def getSubject(self):
+        return self.subject
+    
+    # heeeee
+
+import Item
+import teacher
+class Combat: 
+    def playerattack():
+        
+    def teacherattack():
+        pass
+    def blockoppertunity():
+        pass
+    
